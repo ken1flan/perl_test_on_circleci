fb26d2c5a0ac00a9edf44491d11828cd58beaade
ken1flan <ken1flan@gmail.com>
2019-06-21 23:44:44 +0900

    DBIを使ったテストの追加

--- test.pl
+++ test.pl
@@ -1,6 +1,21 @@
 use strict;
 use warnings;
+use DBI;
 use Test::Simple tests => 2;
 
 ok( 1 + 1 == 2 );
 ok( 2 + 2 == 4 );
+
+my $dbh = DBI->connect(
+  "DBI:MariaDB:dbname=circleci".
+  ";host=127.0.0.1".
+  ";mariadb_connect_timeout=10",
+  "root", ""
+);
+
+my $sth = $dbh->prepare("SELECT * FROM cats WHERE id = ?");
+$sth->execute(1);
+my $hash_ref = $sth->fetchrow_hashref;
+
+ok( $hash_ref->{name} eq "りん" );
+ok( $hash_ref->{color} eq "シャム" );
