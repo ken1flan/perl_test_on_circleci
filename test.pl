9907fc3dae2fae9b989b38f56a05e0702b958dad
ken1flan <ken1flan@gmail.com>
2019-06-24 07:45:08 +0900

    encodingを表示

--- test.pl
+++ test.pl
@@ -3,6 +3,7 @@ use warnings;
 use utf8;
 use open IO => qw/:encoding(UTF-8) :std/;
 use lib "./local/lib/perl5";
+use Encode::Guess qw/cp932 euc-jp 7bit-jis utf8 latin1/;
 use DBI;
 use Test::Simple tests => 2;
 
@@ -20,8 +21,11 @@ $dbh->do("set names sjis");
 my $sth = $dbh->prepare("SELECT * FROM cats WHERE id = ?");
 $sth->execute(1);
 my $hash_ref = $sth->fetchrow_hashref;
-print $hash_ref->{name};
-print "りん";
+
+my $encoding = guess_encoding($hash_ref->{name});
+print "$hash_ref->{name}:$encoding\n";
+my $test_encoding = guess_encoding("りん");
+print "りん:$test_encoding\n";
 
 ok( $hash_ref->{name} eq "りん" );
 ok( $hash_ref->{color} eq "シャム" );
