e79595be2cfe9393e8cbedf6aa3b7b22c6a8ce33
ken1flan <ken1flan@gmail.com>
2019-11-15 07:23:40 +0900

    Add E2E test
    headless chromeを使ったテストを追加しました。
    CircleCI上で動くようには調整していません。

--- test/e2e_test.pl
+++ test/e2e_test.pl
@@ -6,10 +6,10 @@ use lib "./local/lib/perl5";
 use Log::Log4perl qw(:easy);
 use WWW::Mechanize::Chrome;
 
+use Test::Simple tests => 1;
+
 my $mech = WWW::Mechanize::Chrome->new(headless=> 1);
 $mech->get('http://localhost/cgi-bin/index.cgi');
-my $png = $mech->content_as_png();
+my $content = $mech->content;
 
-open my $out, '>', 'outfile.png' or die "Couldn't create png: $!";
-binmode $out;
-print { $out } $png;
+ok( $content =~ /Hello, World/ );
