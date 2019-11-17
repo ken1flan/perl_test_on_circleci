3228e534b0f7dbbad24c8e9710a455343603a66f
ken1flan <ken1flan@gmail.com>
2019-11-17 12:21:42 +0900

    Fix database content
    サンプルアプリの内容に合わせて、
    データベース名、テーブル名、レコード内容を変えた。

--- create_tables.sql
+++ create_tables.sql
@@ -1,30 +1,78 @@
-DROP DATABASE IF EXISTS `circleci`;
-CREATE DATABASE circleci DEFAULT CHARACTER SET utf8;
+DROP DATABASE IF EXISTS `CatsDB`;
+CREATE DATABASE CatsDB DEFAULT CHARACTER SET utf8;
 
-USE circleci;
+USE CatsDB;
 
-DROP TABLE IF EXISTS `Cats`;
-CREATE TABLE cats (
+DROP TABLE IF EXISTS `CatColors`;
+CREATE TABLE CatColors (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(64),
-  color VARCHAR(64),
+  name_en VARCHAR(64),
+  name_jp VARCHAR(64),
   PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO cats (name, color)
+INSERT INTO CatColors (name_en, name_jp)
 VALUES (
-  'りん'
+  'red tabby'
+, '茶トラ'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'silver tabby'
+, 'サバトラ'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'brown tabby'
+, 'キジトラ'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'spotted'
+, 'ヒョウ柄'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'calico'
+, '三毛'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'bicolor'
+, 'ブチ'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'tortoiseshell'
+, 'サビ'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'pointed'
 , 'シャム'
 );
 
-INSERT INTO cats (name, color)
+INSERT INTO CatColors (name_en, name_jp)
 VALUES (
-  'たろう'
-, 'シャム'
+  'black'
+, '黒'
 );
 
-INSERT INTO cats (name, color)
+INSERT INTO CatColors (name_en, name_jp)
 VALUES (
-  'じろう'
-, 'しろ'
+  'white'
+, '白'
+);
+
+INSERT INTO CatColors (name_en, name_jp)
+VALUES (
+  'grey'
+, '灰'
 );
